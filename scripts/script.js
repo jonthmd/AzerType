6fb32fe7c8bfb92110ce4f87245f53b369dba2d6
jon <jonathan.thermidor@outlook.com>
2026-07-02 01:24:32 +0200

    Choix liste sur page HTML.

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,15 @@ function afficherResultat(score, nbTotalMots) {
     spanZoneScore.innerText = message
 }
 
-// function choisirPhrasesOuMots() {
+function choisirPhrasesOuMots() {
 //     let choix = prompt("Quelle liste choisis-tu : 'mots' ou 'phrases' ?")
 //     while (choix !== "mots" && choix !== "phrases") {
 //         choix = prompt("Quelle liste choisis-tu : 'mots' ou 'phrases' ?")
 //     }
 //     return choix
-// }
+
+
+}
 
 // function lancerBoucleDeJeu(liste) {
 //     let score = 0;
@@ -28,6 +30,7 @@ function lancerJeu() {
     let score = 0
     // let nbTotalMots = 0
     let i = 0
+    let listeProposition = listeMots
     // let choix = choisirPhrasesOuMots()
 
     // if (choix === "mots") {
@@ -37,31 +40,44 @@ function lancerJeu() {
     //     score = lancerBoucleDeJeu(listePhrases)
     //     nbTotalMots = listePhrases.length
     // }
-
     let btnValiderMot = document.getElementById("btnValiderMot")
     let inputEcriture = document.getElementById("inputEcriture")
-    afficherProposition(listeMots[i])
+    afficherProposition(listeProposition[i])
     btnValiderMot.addEventListener("click", () => {
+
         console.log(inputEcriture.value)
-        console.log(listeMots[i])
-        if(inputEcriture.value === listeMots[i]) {
+        console.log(listeProposition[i])
+        if (inputEcriture.value === listeProposition[i]) {
             score++
         }
         i++
         afficherResultat(score, i)
         inputEcriture.value = ''
-        if(listeMots[i] === undefined) {
+        if (listeProposition[i] === undefined) {
             afficherProposition("Fin du jeu !")
             btnValiderMot.disabled = true
         } else {
-            afficherProposition(listeMots[i])
+            afficherProposition(listeProposition[i])
         }
     })
 
+    let listeBtnRadio = document.querySelectorAll(".zoneOptions input")
+    for (let ii = 0; ii < listeBtnRadio.length; ii++) {
+        listeBtnRadio[ii].addEventListener("change", (event) => {
+            console.log(event.target.value)
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                afficherProposition(listeProposition[i])
+                listeProposition = listePhrases
+            }
+            afficherProposition(listeProposition[i])
+        })
+    }
     afficherResultat(score, i)
 }
 
-function afficherProposition(proposition){
+function afficherProposition(proposition) {
     let zoneProposition = document.querySelector(".zoneProposition")
     zoneProposition.innerText = proposition
 }
